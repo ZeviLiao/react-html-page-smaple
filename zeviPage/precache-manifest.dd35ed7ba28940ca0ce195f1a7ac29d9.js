41ab90abe4cad4893053830ab0fd726ed7ba7dd3
Zevi Liao <jointjm@gmail.com>
2020-02-13 17:00:50 +0800

    add react page home page in zeviPage

---
+++ zeviPage/precache-manifest.dd35ed7ba28940ca0ce195f1a7ac29d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc0ca158afd1694387b42e0c5e133f6",
+    "url": "/zeviPage/index.html"
+  },
+  {
+    "revision": "adc8d93dfede80260848",
+    "url": "/zeviPage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/zeviPage/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/zeviPage/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc8d93dfede80260848",
+    "url": "/zeviPage/static/js/main.4af97aaf.chunk.js"
+  },
+  {
+    "revision": "ed14983f9cb68eeeeee5",
+    "url": "/zeviPage/static/js/runtime-main.56a78b2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zeviPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
