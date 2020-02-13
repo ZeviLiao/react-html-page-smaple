abf1638aeb7decfcd3bb2f4229728d0d8653b9e1
Zevi Liao <jointjm@gmail.com>
2020-02-13 16:16:12 +0800

    react

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function al(params) {
-    alert('hello')
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return <div>hello</div>
+  }
 }
+
+ReactDOM.render(<CommentBox />, document.getElementById('container'))
